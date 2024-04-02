19bdf1666b306e57d84dfb12a3ede08817d2f873
saydullo07 <ssoibnazarov07@gmail.com>
2024-04-02 22:21:26 +0500

    tesst

---
+++ index.js
@@ -0,0 +1,25 @@
+  //4
+// let str = prompt()
+// let letters = +prompt()
+// function cut(word , letters) {
+//     alert(str.slice(-(letters+1)))
+// }
+// cut(str , letters)
+
+
+
+    //3
+//     let s = prompt()
+//     function something(){
+// console.log('something' + a);
+//     }
+//     something()
+    
+
+    //5
+    // let a = prompt('1-brinch son krtin')
+    // let b = prompt('2-brinch son krtin')
+    // function kara() {
+    //     alert(a * b)
+    // }
+    // kara()
